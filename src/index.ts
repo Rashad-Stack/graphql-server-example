56540e4ffa0f180ab1000fea5af7349cb4d9dd55
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-01-28 06:54:20 +0600

    Add morgan middleware for logging requests

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 import express from "express";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+(async () => {
+  try {
+    const app = express();
+    const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+    // middlewares
+    app.use(express.json());
+    // app.use(morgan("dev"));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    // create a new apollo server instance
+    const server = new ApolloServer({
+      typeDefs: `#graphql
+      type Query {
+        hello: String!
+      }
+      `,
+      resolvers: {
+        Query: {
+          hello: () => "Hello World!",
+        },
+      },
+    });
+    await server.start();
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+
+    app.use("/graphql", expressMiddleware(server));
+
+    app.listen(PORT, () => {
+      console.log(`👍 Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(`😡 Error: ${error}`);
+  }
+})();
